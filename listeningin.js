3200ec0660c1f7342ef545badf42f8947b437a71
nel391 <nel391@gmail.com>
2012-12-24 13:35:20 -0800

    added comments

--- listeningin.js
+++ listeningin.js
@@ -1,4 +1,18 @@
 // listeningin.js
+ /*
+ In this project, you'll be writing a short program 
+ that checks a block of text for your name.
+
+ Specifically, it will check the text for the first 
+ letter of your name, then push the number of 
+ characters equal to your name's length to an array. 
+ By inspecting the array, you'll be able to see if 
+ your name was mentioned!
+
+ Once you've got the hang of loops, arrays, and if 
+ statements, we'll talk about ways this project 
+ could be improved.
+ */
 var text="knvaoen Santing knbo";
 var myName="Santing";
 var hits=[];
