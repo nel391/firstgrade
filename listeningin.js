7c86f19a379ede0d6c8f59da9a8dfd76745d115e
nel391 <nel391@gmail.com>
2012-12-24 13:31:27 -0800

    modified

--- listeningin.js
+++ listeningin.js
@@ -1,15 +1,4 @@
-//listeningin.js
-//
-/*In this project, you'll be writing a short program that \
- * checks a block of text for your name. Specifically, it will \
- * check the text for the first letter of your name, then push \
- * the number of characters equal to your name's length to an array. \
- * By inspecting the array, you'll be able to see if your name \
- * was mentioned! Once you've got the hang of loops, arrays, \
- * and if statements, we'll talk about ways this project could \
- * be improved. */
-
-
+// listeningin.js
 var text="knvaoen Santing knbo";
 var myName="Santing";
 var hits=[];
@@ -22,9 +11,8 @@ for(i=0;i<=text.length;i++){
     if(text[i]==myName[0]){
         console.log("BOOM! -> text[i] = myName[0]");
         console.log(text[i] + " = " + myName[0]);
-        
-            hits.push(text[i]);
-      
+        hits.push
+
         console.log(hits);
     }
 }
